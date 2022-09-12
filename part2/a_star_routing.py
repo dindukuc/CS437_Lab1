0dc0ee36eae930e5e4f9520b12c3ea848b7874cf
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-11 23:02:27 -0700

    still writing a_star routing

--- part2/a_star_routing.py
+++ part2/a_star_routing.py
@@ -11,6 +11,7 @@ class Point:
     y = -1
     key = ""
     value = -1
+    successors = []
 
     def __init__(self, x, y, value):
         
@@ -19,45 +20,60 @@ class Point:
         self.value = int(value)
         self.key = str(x) + "," + str(y)
 
+    def __eq__(self, other):
+        return self.key == other.key
+
+
+
 
 def convert_grid(grid):
-    closed = {}
+    all_nodes_list = {}
 
     temp = None
 
     for idx, val in np.ndenumerate(grid):
         temp = Point(idx[0], idx[1], val)
-        closed[temp.key] = temp.value
+        all_nodes_list[temp.key] = temp.value
         temp = None
         # break;
 
-    # print(closed["99,99"])
-    return closed
+    # print(all_nodes_list["99,99"])
+    return all_nodes_list
 
 def manhattan_dist(point1, point2):
     dist = abs(point1.x - point2.x) + abs(point1.y - point2.y)
 
     return dist
 
-
-
-
-def a_star():
+def compute_successors(node):
+    # successors = 
     pass;
+
+
+
+
+def a_star(all_nodes_list, start, goal):
+    open = []
+    open.append(start, 0)
+
+    while open:
+        open.sort(key= lambda tup: tup[1])
+        curr_node = open.pop(0)
 
 
 
 
 
 if __name__ == "__main__":
-    start = Point(49, 9, 0)
-    goal = Point(99, 99, 0)
+    start = Point(50, 0, 0)
+    goal = Point(89, 89, 0)
     
-    grid = np.zeros((100,100))
-    grid[99,99] = 1
+    grid = np.zeros((90,90))
+    # grid[99,99] = 1
 
-    closed = convert_grid(grid)
-    a_star(closed)
+
+    all_nodes_list = convert_grid(grid)
+    # a_star(all_nodes_list, start, goal)
 
 
     # plt.imshow(grid, origin="lower")
