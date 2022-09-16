145e8337736da64f1cee54600504095322be2aaf
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-16 13:14:03 -0700

    moved stop sign functions

---
+++ part2/object_test.py
@@ -0,0 +1,10 @@
+import stop_sign_detection as st
+
+
+
+while True:
+    if st.detect_stop_sign(1) == True:
+        print("detected stop sign!")
+    else:
+        print("No stop sign detected")
+
