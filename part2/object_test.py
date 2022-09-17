9018ea9d230f15a13aa0b98491980264111971f6
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-16 22:32:23 -0700

    Trying stop sign function

--- part2/object_test.py
+++ part2/object_test.py
@@ -2,8 +2,25 @@ import sys
 import stop_sign_detection as st
 
 
-while True:
-    if st.detect_stop_sign(1) == True:
-        print("detected stop sign!")
-    else:
-        print("No stop sign detected")
+
+
+def detect():
+    while True:
+        if st.detect_stop_sign(1) == True:
+            print("detected stop sign!")
+        else:
+            print("No stop sign detected")
+
+
+
+
+if __name__ == "__main__":
+    
+    while True:
+        try:
+            if st.detect_stop_sign(1) == True:
+                print("detected stop sign!")
+            else:
+                print("No stop sign detected")
+        except:
+            print()
