c063666121d66a2a99654733bb9883c99dd84a66
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-17 11:06:03 -0700

    trying to suppress error message

--- part2/object_test.py
+++ part2/object_test.py
@@ -17,10 +17,14 @@ def detect():
 if __name__ == "__main__":
     
     while True:
+        time.sleep(1)
         try:
             if st.detect_stop_sign(30) == True:
                 print("DETECTED STOP SIGN!")
+                exit()
             else:
                 print("No stop sign detected")
         except AttributeError:
             print("Caught exception from attribute error!")
+
+    
