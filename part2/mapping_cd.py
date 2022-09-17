83e10b26c82c28e9dfba50b3d84bdd56557191c3
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-17 15:50:33 -0700

    testing mapping function

--- part2/mapping_cd.py
+++ part2/mapping_cd.py
@@ -4,22 +4,46 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+
+
+
+
+
+def measure_dist():
+    obj_dist = []
+    for i in range(-80, 90, 10):
+        dist_val = fc.get_distance_at(i)
+        time.sleep(.05)
+        obj_dist.append((i, dist_val))
+    
+    print(obj_dist)
+    return obj_dist
+
+
+
+
+
+
+
+def mapping(grid, curr_pos):
+    pass;
+
+
+
+
 def main():
     grid = np.array((90, 100))
+    curr_pos = (50, 0)
+    grid = mapping(grid, curr_pos)
 
-
-
-
-
-
-
-
+    plt.imshow(grid, origin="lower")
+    plt.show()
 
 
 
 
 if __name__ == "__main__":
     try:
-        main()
+        measure_dist()
     finally:
         fc.stop()
