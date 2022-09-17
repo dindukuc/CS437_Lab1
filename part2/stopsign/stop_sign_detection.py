4c8ac73ae26268265f945dff6b821292869db355
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-16 22:07:44 -0700

    import module error in stop sign detect

--- part2/stopsign/stop_sign_detection.py
+++ part2/stopsign/stop_sign_detection.py
@@ -6,7 +6,7 @@ import cv2
 from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
-import utils
+from stopsign import utils
 
 
 def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
