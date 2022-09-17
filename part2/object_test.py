86b37badf104c284bf6ec57f95ba3ac19a799215
dindukuc <chetan.dindukurthi@gmail.com>
2022-09-17 11:27:53 -0700

    going to redirect errors to a txt file

--- part2/object_test.py
+++ part2/object_test.py
@@ -6,26 +6,23 @@ import time
 
 
 def detect():
-    while True:
-        if st.detect_stop_sign(1) == True:
-            print("detected stop sign!")
-        else:
-            print("No stop sign detected")
+    
+    time.sleep(1)
+    if st.detect_stop_sign(15) == True:
+        print("DETECTED STOP SIGN! EXITING PROGRAM.....")
+        exit()
+    else:
+        print("No stop sign detected")
 
 
 
 
 if __name__ == "__main__":
     
-    while True:
-        time.sleep(1)
-        try:
-            if st.detect_stop_sign(30) == True:
-                print("DETECTED STOP SIGN!")
-                exit()
-            else:
-                print("No stop sign detected")
-        except AttributeError:
-            print("Caught exception from attribute error!")
-
+    try:
+        while True:
+            detect()
+    except:
+        print("Some error detected...read log.txt")
+        exit()
     
